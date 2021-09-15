6c2849929b5962bb9cf461ef7386e79e943bb1ee
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-15 12:28:48 +0200

    Add Top-Nav

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <div class="container">
+      <TopNav />
+      <div id="bottom-nav" class="row"></div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import TopNav from './TopNav.vue';
+
+export default {
+  name: 'Header',
+  components: {
+    TopNav,
+  },
+};
+</script>
+
+<style scoped>
+#bottom-nav {
+  height: 105px;
+  background-color: goldenrod;
+}
+</style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="top-nav" class="row justify-content-between">
+    <!-- SOCIAL ICONS -->
+    <div class="col-2 d-flex align-items-center">
+      <a
+        v-for="social in socialLinks"
+        :key="social"
+        class="icon"
+        :href="social.link"
+      >
+        <i class="fab" :class="social.icon"></i>
+      </a>
+    </div>
+    <!-- ACCOUNT & CART LINKS -->
+    <div class="col-6 d-flex align-items-center">
+      <div class="col-6 account-links">
+        <a href="https://vuejs.org/">Shopping Card</a>
+        <a href="#">My Account<i class="fas fa-chevron-down"></i> </a>
+      </div>
+      <div class="cart col-6 d-flex align-items-center justify-content-center">
+        <a href="#"
+          ><i class="fas fa-shopping-cart"></i>CART
+          <i class="fas fa-chevron-down"></i
+        ></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopNav',
+  data() {
+    return {
+      socialLinks: [
+        { icon: 'fa-facebook-f', link: 'https://vuejs.org/' },
+        { icon: 'fa-twitter', link: 'https://vuejs.org/' },
+        { icon: 'fa-instagram', link: 'https://vuejs.org/' },
+        { icon: 'fa-youtube', link: 'https://vuejs.org/' },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+#top-nav {
+  max-width: 950px;
+  height: 45px;
+  margin: 0 auto;
+}
+.icon {
+  margin: 0 10px;
+}
+.account-links a {
+  font-size: 0.8rem;
+  margin: 0 10px;
+}
+.cart {
+  height: 100%;
+  border-left: 1px solid #eeeeee;
+  background-color: #e5e5e5;
+  font-size: 0.8rem;
+}
+.fa-chevron-down {
+  margin-left: 10px;
+}
+.fa-shopping-cart {
+  margin-right: 10px;
+}
+</style>
